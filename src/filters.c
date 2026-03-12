6782959d504580e160fed37da0ff7d00baba3fdc
MVT2911 <mvarasu1999@gmail.com>
2026-03-12 16:36:51 -0300

    Addition of Sobel Edge Detection to filters

--- src/filters.c
+++ src/filters.c
@@ -31,3 +31,23 @@ void blur_serial(unsigned char* input, unsigned char* output, int width, int hei
       }
   }
 }
+// Sobel edge detection filter implementation
+void sobel_serial(unsigned char* input, unsigned char* output, int width, int height) {
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+ 
+    for (int y = 1; y < height - 1; y++) {
+        for (int x = 1; x < width - 1; x++) {
+            float sumX = 0, sumY = 0;
+            for (int ky = -1; ky <= 1; ky++) {
+                for (int kx = -1; kx <= 1; kx++) {
+                    int val = input[IX(x + kx, y + ky, width)];
+                    sumX += val * Gx[ky + 1][kx + 1];
+                    sumY += val * Gy[ky + 1][kx + 1];
+                }
+            }
+            int mag = (int)sqrt(sumX * sumX + sumY * sumY);
+            output[IX(x, y, width)] = (mag > 255) ? 255 : mag;
+        }
+    }
+  }
