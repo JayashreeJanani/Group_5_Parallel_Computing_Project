3cb7e4bbf3a0175a7974ab97b34855246b8ac62d
JayashreeJanani <jayashree.199731@gmail.com>
2026-03-12 14:07:23 -0300

    Gaussian blur filter implemention with 3by3 kernel

--- src/filters.c
+++ src/filters.c
@@ -11,3 +11,23 @@ void grayscale_serial(unsigned char* input, unsigned char* output, int width, in
     output[i] = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
   }
 }
+// Gaussian blur filter implementation (3x3 kernel)
+void blur_serial(unsigned char* input, unsigned char* output, int width, int height) {
+  float kernel[3][3] = {
+        {1/16.0, 2/16.0, 1/16.0},
+        {2/16.0, 4/16.0, 2/16.0},
+        {1/16.0, 2/16.0, 1/16.0}
+    };
+ 
+  for (int y = 1; y < height - 1; y++) {
+      for (int x = 1; x < width - 1; x++) {
+          float sum = 0.0;
+          for (int ky = -1; ky <= 1; ky++) {
+              for (int kx = -1; kx <= 1; kx++) {
+                  sum += input[IX(x + kx, y + ky, width)] * kernel[ky + 1][kx + 1];
+              }
+          }
+          output[IX(x, y, width)] = (unsigned char)sum;
+      }
+  }
+}
